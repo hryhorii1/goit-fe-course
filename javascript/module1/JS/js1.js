fd3848b4c01f340276214c8d0e7c6652dd2bfd0f
Hryhorii Prudkyi <grygorii.prudkyi@gmail.com>
2018-12-24 22:53:20 +0200

    homework1-improvements

--- javascript/module1/JS/js1.js
+++ javascript/module1/JS/js1.js
@@ -1,25 +1,25 @@
+'use strict';
+
 const adminLogin = 'admin';
 const adminPassword = 'm4ngo1zh4ackz0r';
-const $cansel = 'Відмінено користувачем!';
-const $accessDenied = 'Доступ заборонено!';
-const $welcome = 'Ласкаво просимо!';
+const cansel = 'Відмінено користувачем!';
+const accessDenied = 'Доступ заборонено!';
+const welcome = 'Ласкаво просимо!';
+const user = prompt('Введіть ваш логін');
 
-const $login = prompt('Enter Login');
-if ($login === null) {
-    alert($cansel)
-} else {
-    if ($login !== adminLogin) {
-        alert($accessDenied)
+console.log(user);
+
+if (user === null) {
+    alert(cansel);
+} else if (adminLogin === user) {
+    const userPassword = prompt('Введіть ваш пароль');
+    if (userPassword === null) {
+        alert(cansel);
+    } else if (userPassword === adminPassword) {
+        alert('Ласкаво просимо!')
     } else {
-        const $password = prompt('Enter password')
-        if ($password === null) {
-            alert($cansel)
-        } else {
-            if ($password !== adminPassword) {
-                alert($accessDeniend)
-            } else {
-                alert($welome);
-            }
-        }
+        alert('Помилка при вході! Не вірний пароль!')
     }
+} else {
+    alert('Доступ заборонено!')
 }
