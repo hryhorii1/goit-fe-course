ca7a0a1b8a03676fd1313ccd7b4305acad1d256a
Hryhorii Prudkyi <grygorii.prudkyi@gmail.com>
2018-12-30 20:04:55 +0200

    Homework-2 new

--- javascript/module2/JS/js1.js
+++ javascript/module2/JS/js1.js
@@ -1,23 +1,22 @@
 'use strict'
 
+let userArr = [];
 let userInput;
-const numbers = [];
 let total = 0;
 
 do {
-    userInput = prompt('Введіть число');
-    if (isNaN(userInput)) {
-        alert('Було введено не число, спробуйте ще раз')
-    } else {
-        numbers.push(Number(userInput));
-    }
-} while (userInput != null);
+    userArr.push(Number(userInput = prompt('Введіть число масива')));
+} while (Number(userInput) > 0);
 
-for (let i = 0; i < numbers.length; i += 1) {
-    total += numbers[i];
+for (const i of userArr) {
+    if (isNaN(total += i)) {
+        alert('Було введено не число, спробуйте ще раз');
+        break;
+    }
 }
-if (total !== 0) {
-    alert(`Загальна сума чисел рівна ${total}`);
-} else {
-    alert('Масив пустий!');
+
+if (total > 0) {
+    alert(`Сума всіх значень в масиві: ${total}`);
 }
+
+console.log(total);
